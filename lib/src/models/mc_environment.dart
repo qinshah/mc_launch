df5d4d148c3ccacfb171ccb416496462db401cdf
qshh <3218696007@qq.com>
2025-09-14 16:08:02 +0800

    fix(models): 临时使用只有纯净mc的.minecraft文件夹路径

--- lib/src/models/mc_environment.dart
+++ lib/src/models/mc_environment.dart
@@ -1,7 +1,7 @@
 /// Minecraft 环境配置
 class McEnvironment {
   /// .minecraft 文件夹路径
-  static const String minecraftPath = '/Users/qshh/Desktop/FML/.minecraft';
+  static const String minecraftPath = '/Users/qshh/Desktop/Dev/FA/unknown_studio/.minecraft';
   
   /// Java 可执行文件路径
   static const String javaPath = 'java';
